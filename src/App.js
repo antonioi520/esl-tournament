373ef58538323b060a884d8d9950dfb8d5ac8d43
antonio <antonioi19961@gmail.com>
2021-08-19 05:21:23 -0400

    navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter, Link, Route, Redirect } from 'react-router-dom';
+import Search from '../src/Components/Search.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <Route exact path='/' component={Search} />
+        </BrowserRouter>
+    );
+  }
 }
+
+
 
 export default App;
